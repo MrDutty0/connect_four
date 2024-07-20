f616fe679e6c0ce53eff4c2d6a867596bfeddaef
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2024-07-20 17:07:09 +0300

    Add horizontal checking for game state

--- lib/board.rb
+++ lib/board.rb
@@ -30,8 +30,28 @@ class Board
     winner_in_row?(row)
   end
 
-  # def horizontal_win?(last_move)
-  # end
+  def horizontal_win?(last_move)
+    i, j = last_move
+    current_piece = board[i][j]
+
+    piece_occurrences = 1
+
+    # Check left
+    (j - 1).downto(0) do |left|
+      break if board[i][left] != current_piece
+
+      piece_occurrences += 1
+    end
+
+    # Check right
+    (j + 1).upto(ROW_LENGHT - 1) do |right|
+      break if board[i][right] != current_piece
+
+      piece_occurrences += 1
+    end
+
+    piece_occurrences >= WINNING_ROW_LENGHT
+  end
 
   # def diagonal_win?(last_move)
   # end
@@ -42,3 +62,4 @@ class Board
     row.compact.size == WINNING_ROW_LENGHT && row.uniq.size == 1
   end
 end
+
