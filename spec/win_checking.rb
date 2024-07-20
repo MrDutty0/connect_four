436984d6306b5a4fc659fa4cc065db3333c09a19
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2024-07-20 17:05:16 +0300

    Small refactoring, add tests for horizontal win checking
    Used inside shared examples variable that holds testing method name so it
    can be used in multiple contexts

--- spec/win_checking.rb
+++ spec/win_checking.rb
@@ -2,13 +2,13 @@
 
 require_relative '../lib/board'
 
-RSpec.shared_examples 'winning checking' do |last_move, board_state, expected_outcome|
+RSpec.shared_examples 'winning checking' do |testing_method, last_move, board_state, expected_outcome|
   before do
     allow(game_board).to receive(:board).and_return(board_state)
   end
 
   it "returns #{expected_outcome}" do
-    state = game_board.vertical_win?(last_move)
+    state = game_board.send(testing_method, last_move)
     expect(state).to eql(expected_outcome)
   end
 end
@@ -17,8 +17,10 @@ describe Board do
   subject(:game_board) { described_class.new }
 
   describe '#vertical_win?' do
+    testing_method = :vertical_win?
+
     context 'when there is no winning vertically' do
-      include_examples 'winning checking', [1, 0], [
+      include_examples 'winning checking', testing_method, [1, 0], [
         [nil, nil, nil, nil, nil, nil, nil],
         ['X', nil, nil, nil, nil, nil, nil],
         ['X', nil, nil, nil, nil, nil, nil],
@@ -29,7 +31,7 @@ describe Board do
     end
 
     context 'when there are not enough placed symbols' do
-      include_examples 'winning checking', [3, 1], [
+      include_examples 'winning checking', testing_method, [3, 1], [
         [nil, nil, nil, nil, nil, nil, nil],
         [nil, nil, nil, nil, nil, nil, nil],
         [nil, nil, nil, nil, nil, nil, nil],
@@ -38,15 +40,52 @@ describe Board do
         [nil, 'X', nil, nil, nil, nil, nil]
       ], false
     end
+  end
 
-    context 'when won vertically' do
-      include_examples 'winning checking', [0, 1], [
-        [nil, 'X', nil, nil, nil, nil, nil],
-        [nil, 'X', nil, nil, nil, nil, nil],
-        [nil, 'X', nil, nil, nil, nil, nil],
-        [nil, 'X', nil, nil, nil, nil, nil],
-        [nil, 'O', nil, nil, nil, nil, nil],
-        [nil, 'X', nil, nil, nil, nil, nil]
+  describe '#horizontal_win?' do
+    testing_method = :horizontal_win?
+
+    context 'when piece is the left most 4th of the same type' do
+      include_examples 'winning checking', testing_method, [5, 1], [
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        ['X', 'O', 'O', 'O', 'O', nil, nil]
+      ], true
+    end
+
+    context 'when piece is the right most 4th of the same type' do
+      include_examples 'winning checking', testing_method, [5, 4], [
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        ['X', 'O', 'O', 'O', 'O', nil, nil]
+      ], true
+    end
+
+    context 'when piece is between the same type of pieces' do
+      include_examples 'winning checking', testing_method, [5, 2], [
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        ['X', 'O', 'O', 'O', 'O', nil, nil]
+      ], true
+    end
+
+    context 'when last placed piece is between the same type pieces' do
+      include_examples 'winning checking', testing_method, [5, 2], [
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        [nil, nil, nil, nil, nil, nil, nil],
+        ['X', 'O', 'O', 'O', 'O', nil, nil]
       ], true
     end
   end
