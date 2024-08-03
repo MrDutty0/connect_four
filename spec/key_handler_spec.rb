4b6154741564653f2d1950f1cf8dc84e2ac8432d
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2024-08-03 17:34:43 +0300

    feat: add tests for 'handle_key_press' method

--- spec/key_handler_spec.rb
+++ spec/key_handler_spec.rb
@@ -50,4 +50,48 @@ describe KeyHandler do
       end
     end
   end
+
+  describe '#handle_key_press' do
+    let(:move) { [1, 1] }
+
+    context 'when pressed key is invalid' do
+      let(:invalid_key) { 'e' }
+
+      it 'displays an error message and returns previous move' do
+        error_msg = 'Invalid pressed key'
+
+        expect do
+          cli_dummy.handle_key_press(invalid_key, move)
+        end.to change {
+          cli_dummy.instance_variable_get(:@error_msg)
+        }.from(nil).to(error_msg)
+
+        result = cli_dummy.handle_key_press(invalid_key, move)
+        expect(result).to eq(move)
+      end
+    end
+
+    context 'when pressed the enter key' do
+      let(:enter_key) { Constants::ENTER_KEY }
+
+      it 'returns the move' do
+        result = cli_dummy.handle_key_press(enter_key, move)
+        expect(result).to eq(move)
+      end
+    end
+
+    context 'when pressed the left arrow key' do
+      let(:valid_key) { Constants::LEFT_KEY }
+      let(:updated_move) { [1, 0] }
+
+      before do
+        allow(cli_dummy).to receive(:update_move).and_return(updated_move)
+      end
+
+      it 'returns the updated move' do
+        result = cli_dummy.handle_key_press(valid_key, move)
+        expect(result).to eq(updated_move)
+      end
+    end
+  end
 end
