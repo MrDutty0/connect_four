8597e70f4492958e14c0fe4cb3b8581970e7f75f
Tomas Civilis <96726777+MrDutty0@users.noreply.github.com>
2024-08-05 22:38:17 +0300

    feat: tests for '#retrieve_move', change test messages

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -10,6 +10,7 @@ describe CLI do
 
   context '#retrieve_move' do
     let(:initial_position) { [Constants::COLUMN_LENGTH / 2, Constants::ROW_LENGTH / 2] }
+    let(:error_msg) { 'Invalid pressed key' }
 
     before do
       allow(cli).to receive(:initial_move_position).and_return(initial_position)
@@ -27,9 +28,7 @@ describe CLI do
           allow(cli).to receive(:prompt_for_single_char).and_return(invalid_key, valid_key)
         end
 
-        it 'completes loop, displays error message once and returns move' do
-          error_msg = 'Invalid pressed key'
-
+        it 'displays error message once and returns move' do
           expect(cli).to receive(:puts).with(error_msg).once
 
           result = cli.retrieve_move(board, player_name)
@@ -37,7 +36,7 @@ describe CLI do
         end
       end
 
-      context 'first press' do
+      context 'on first press' do
         before do
           valid_key = Constants::ENTER_KEY
           allow(cli).to receive(:prompt_for_single_char).and_return(valid_key)
@@ -46,6 +45,36 @@ describe CLI do
         it 'returns move after one loop iteration' do
           expect(cli).to receive(:prompt_for_single_char).once
 
+          result = cli.retrieve_move(board, player_name)
+          expect(result).to eq(expected_move_outcome)
+        end
+      end
+    end
+
+    context 'when moving key is pressed' do
+      let(:expected_move_outcome) { [initial_position[0], initial_position[1] - 1] }
+      context 'after non-valid key and then moving and enter keys' do
+        before do
+          invalid_key = '#'
+          valid_keys = [Constants::LEFT_KEY, Constants::ENTER_KEY]
+          allow(cli).to receive(:prompt_for_single_char).and_return(invalid_key, *valid_keys)
+        end
+
+        it 'displays error, changes position to left and returns move' do
+          expect(cli).to receive(:puts).with(error_msg).once
+
+          result = cli.retrieve_move(board, player_name)
+          expect(result).to eq(expected_move_outcome)
+        end
+      end
+
+      context 'directly moving key and then enter key' do
+        before do
+          valid_keys = [Constants::LEFT_KEY, Constants::ENTER_KEY]
+          allow(cli).to receive(:prompt_for_single_char).and_return(*valid_keys)
+        end
+
+        it 'changes position to left and returns move' do
           result = cli.retrieve_move(board, player_name)
           expect(result).to eq(expected_move_outcome)
         end
